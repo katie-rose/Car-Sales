afa7d3fe45e2ced55d1bf452ce007ead98b9b1c1
Katie Rose <katiefelten@gmail.com>
2019-07-24 16:06:35 -0400

    :rocket: added actions and reducers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from './App';
+// CSS
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
 const store = createStore(() => {});
-
-import 'bulma/css/bulma.css';
-import './styles.scss';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
