a77a24c46e24934e3d8db66a4868b6ab9e384fd6
Katie Rose <katiefelten@gmail.com>
2019-07-24 15:13:35 -0400

    :heavy_plus_sign: adding the redux package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from './App';
+
+const store = createStore(() => {});
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
